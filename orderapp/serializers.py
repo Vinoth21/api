fadaaf9ad3a18e3a2230387641eb14dd69ec7ffd
vinoth kumar <vinothkumarmvm@gmail.com>
2019-03-01 16:06:41 +0530

    Modified

--- orderapp/serializers.py
+++ orderapp/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from orderapp.models import Menu, UserObjects
 from django.contrib.auth.models import User
-
+from datetime import date
 
 class MenuSerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,13 +14,21 @@ class UserObjectSerializer(serializers.ModelSerializer):
         model = UserObjects
         user = serializers.ReadOnlyField(source='owner.username')
         fields = ('item', 'user', 'like_status', 'date_liked')
-
+"""
+        if self.request.user:
+            menu = Menu.objects.all()
+            for userobj in model.objects.all():
+                if (userobj.like_status == True and userobj.date_liked == date.today):
+                    menu.votes += 1
+"""
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Menu
         #quantity = serializers.ReadOnlyField()
         fields =('item', 'price')
+
+
 
 """
 
